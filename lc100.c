3df7e050c683ea15badaf62e0430c376fd564bbb
anran <370654630@qq.com>
2016-01-29 10:36:58 +0800

    lc100

---
+++ lc100.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+    if(p == NULL && q == NULL)
+        return true;
+    else if(p != NULL && q != NULL && p->val == q->val)
+    {
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+    else
+        return false;
+
+}
